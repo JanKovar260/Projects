89289ca690f5f6f585ff5fad4367af8738c8a2a6
JanKovar260 <71223253+JanKovar260@users.noreply.github.com>
2020-11-27 15:07:01 +0100

    Update format_a_string_of_names.py

--- Codewars/format_a_string_of_names.py
+++ Codewars/format_a_string_of_names.py
@@ -1,3 +1,6 @@
+"""Given: an array containing hashes of names; Return: a string formatted as a list of names 
+separated by commas except for the last two names, which should be separated by an ampersand."""
+
 def namelist(names):
     just_names = []
     for d in names:
@@ -14,6 +17,3 @@ def namelist(names):
     else:
         result = result
     return result
-    
-    """Given: an array containing hashes of names; Return: a string formatted as a list of names 
-    separated by commas except for the last two names, which should be separated by an ampersand."""
