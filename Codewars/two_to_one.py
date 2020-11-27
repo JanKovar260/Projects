6016f733392c1e62041b1e2cd4ea8d0c44917f2c
JanKovar260 <71223253+JanKovar260@users.noreply.github.com>
2020-11-27 15:05:26 +0100

    Update two_to_one.py

--- Codewars/two_to_one.py
+++ Codewars/two_to_one.py
@@ -1,3 +1,8 @@
+"""
+Take 2 strings s1 and s2 including only letters from ato z. 
+Return a new sorted string, the longest possible, containing distinct letters, each taken only once - coming from s1 or s2.
+"""
+
 def longest(s1, s2):
     s = s1+s2
     result = ""
@@ -7,9 +12,6 @@ def longest(s1, s2):
     return "".join(sorted(result))
 
 """
-Take 2 strings s1 and s2 including only letters from ato z. 
-Return a new sorted string, the longest possible, containing distinct letters, each taken only once - coming from s1 or s2.
-
 a = 'xyaabbbccccdefww'
 b = 'xxxxyyyyabklmopq'
 longest(a, b) -> 'abcdefklmopqwxy'
